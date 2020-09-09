05a37182bf3edd33349440ec0982b0b067ac4c55
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-09-09 17:59:05 -0300

    First Commit. Added Delivery Project (Order, Marketplace, Registration projects) + first docker-compose

---
+++ delivery/marketplace/src/test/java/ca/com/rlsp/delivery/NativemarketplaceIT.java
@@ -0,0 +1,9 @@
+package ca.com.rlsp.delivery;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativemarketplaceIT extends marketplaceTest {
+
+    // Execute the same tests but in native mode.
+}
