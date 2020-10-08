bf6e724d3f86b388bcebebf4a3700654584c1ca5
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-10-08 19:49:51 -0300

    Added logstash(centralized log app) for all appls (marketplace, order and registration) and Microprofile Health

--- delivery/order/src/main/java/ca/com/rlsp/delivery/order/HelloResource.java
+++ delivery/order/src/main/java/ca/com/rlsp/delivery/order/HelloResource.java
@@ -1,16 +1,24 @@
 package ca.com.rlsp.delivery.order;
 
+import java.time.LocalDateTime;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.logging.Logger;
+
 @Path("/hello")
 public class HelloResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
-    }
+	  private static final Logger LOG = Logger.getLogger(HelloResource.class);
+
+	    @GET
+	    @Produces(MediaType.TEXT_PLAIN)
+	    public String hello() {
+	        LOG.info("Olá Quarkus");
+	        LOG.infov("Olá Quarkus {0} ", LocalDateTime.now());
+	        return "hello";
+	    }
 }
