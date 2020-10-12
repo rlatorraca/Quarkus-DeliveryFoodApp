97454159d975680b67ac60e489ef693fb90dc737
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-10-12 14:40:02 -0300

    Added a simple example how to use gRPC and Quarkus

---
+++ delivery/grpc/src/main/java/ca/com/rlsp/delivery/grpc/HelloService.java
@@ -0,0 +1,43 @@
+package ca.com.rlsp.delivery.grpc;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import ca.com.rlsp.proto.HelloRequest;
+import ca.com.rlsp.proto.HelloResponse;
+import ca.com.rlsp.proto.HelloServiceGrpc.HelloServiceBlockingStub;
+import ca.com.rlsp.proto.MutinyHelloServiceGrpc.MutinyHelloServiceStub;
+import io.quarkus.grpc.runtime.annotations.GrpcService;
+import io.smallrye.mutiny.Uni;
+
+@Path("/ola")
+public class HelloService {
+
+    @Inject
+    @GrpcService("hello-service")
+    HelloServiceBlockingStub olaService;
+
+    @Inject
+    @GrpcService("hello-service")
+    MutinyHelloServiceStub helloServiceMutiny;
+
+    @GET
+    @Path("/block/{nome}")
+    public String getBlock(@PathParam("nome") String nome) {
+        HelloRequest request = HelloRequest.newBuilder().setNome(nome).build();
+		HelloResponse response = olaService.digaOla(request);
+        return response.getMensagem()+", Quantidade: "+response.getQuantidadeDeChamadas();
+    }
+
+   
+	@GET
+    @Path("/reativo/{nome}")
+    public Uni<String> getReativo(@PathParam("nome") String nome) {
+    	HelloRequest request = HelloRequest.newBuilder().setNome(nome).build();
+        Uni<HelloResponse> response = helloServiceMutiny.digaOla(request);
+        return response.onItem().transform(i -> i.getMensagem()+", Quantidade: "+i.getQuantidadeDeChamadas());
+    }
+
+}
---
+++ delivery/grpc/src/main/java/ca/com/rlsp/delivery/grpc/MyHelloService.java
@@ -0,0 +1,41 @@
+package ca.com.rlsp.delivery.grpc;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.inject.Singleton;
+
+import ca.com.rlsp.proto.HelloRequest;
+import ca.com.rlsp.proto.HelloResponse;
+import io.smallrye.mutiny.Uni;
+
+// Se quiser trabalhar sem o Mutiny
+
+//@Singleton
+//public class MyHelloService extends ca.com.rlsp.proto.HelloServiceGrpc.HelloServiceImplBase {
+//
+//	AtomicInteger inteiro = new AtomicInteger();
+//
+//	public void digaOla(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+//		String nome = request.getNome();
+//
+//		responseObserver.onNext(HelloResponse.newBuilder().setMensagem("Ola " + nome)
+//				.setQuantidadeDeChamadas(inteiro.getAndIncrement()).build());
+//		responseObserver.onCompleted();
+//	}
+//
+//}
+
+//Trabalhando com Mutiny
+
+//@Singleton
+public class MyHelloService extends ca.com.rlsp.proto.MutinyHelloServiceGrpc.HelloServiceImplBase {
+
+    AtomicInteger inteiro = new AtomicInteger();
+        @Override
+        public Uni<HelloResponse> digaOla(HelloRequest request) {
+        	HelloResponse response = HelloResponse.newBuilder()
+                    .setMensagem("Hello-2 "+request.getNome())
+                    .setQuantidadeDeChamadas(inteiro.getAndIncrement()).build();
+			return Uni.createFrom().item(response);
+        }
+}
